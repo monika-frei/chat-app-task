e136029f2272f33828890bc8d74140e99d0d8344
Monika <frei.monika@gmail.com>
2021-03-14 16:04:31 +0100

    added sending message

--- src/ChatView/Chat/Chat.js
+++ src/ChatView/Chat/Chat.js
@@ -1,8 +1,12 @@
 import React, { useState, useCallback, useEffect } from "react";
 import { GiftedChat } from "react-native-gifted-chat";
+import { useMutation } from "@apollo/client";
+import { SEND_MESSAGE } from "../../queries/index";
 
-const Chat = ({ data, subscribeToMoreMessages }) => {
+const Chat = ({ roomId, data, subscribeToMoreMessages }) => {
   const [messages, setMessages] = useState([]);
+  const [sendMessage] = useMutation(SEND_MESSAGE);
+  const [user, setUser] = useState("");
 
   useEffect(() => {
     subscribeToMoreMessages();
@@ -25,12 +29,16 @@ const Chat = ({ data, subscribeToMoreMessages }) => {
         };
       });
     setMessages(array);
+    data && setUser(data.room.user.id);
   }, [data]);
 
   const onSend = useCallback((messages = []) => {
-    setMessages((previousMessages) =>
-      GiftedChat.append(previousMessages, messages)
-    );
+    sendMessage({
+      variables: {
+        text: messages[0].text,
+        roomId: roomId,
+      },
+    });
   }, []);
 
   return (
@@ -38,7 +46,7 @@ const Chat = ({ data, subscribeToMoreMessages }) => {
       messages={messages}
       onSend={(messages) => onSend(messages)}
       user={{
-        _id: 1,
+        _id: user,
       }}
     />
   );
